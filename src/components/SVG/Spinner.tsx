504e6c71aa3e3fe92beeb8b04ccb3af312f97231
Woojin Lee <nijoow1127@gmail.com>
2023-04-20 22:57:02 +0900

    💄 Spinner 컴포넌트 수정

--- src/components/SVG/Spinner.tsx
+++ src/components/SVG/Spinner.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Spinner = () => {
   return (
     <svg
-      className="w-8 h-8 text-white animate-spin"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
       viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-6 h-6 text-white fill-white"
     >
-      <circle
-        className="opacity-100"
-        cx="12"
-        cy="12"
-        r="10"
-        stroke="#3EB5FF"
-        strokeWidth="4"
-      />
-      <path
-        className="opacity-75"
-        fill="#EAF4FF"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      />
+      <circle className="spinner_S1WN" cx="4" cy="12" r="3" />
+      <circle className="spinner_S1WN spinner_Km9P" cx="12" cy="12" r="3" />
+      <circle className="spinner_S1WN spinner_JApP" cx="20" cy="12" r="3" />
     </svg>
   );
 };
